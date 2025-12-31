0bfe6f5da5693699134889406abc91b42424dffa
r9r-dev <bold.spot3022@lamour.bzh>
2025-12-31 12:34:05 +0100

    Bump version to 1.7.10

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # App version
-    app_version: str = "1.7.9"
+    app_version: str = "1.7.10"
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent
