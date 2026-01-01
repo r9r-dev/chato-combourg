4cf213a77fa627ccb09e7b90c81f296c399235a2
r9r-dev <bold.spot3022@lamour.bzh>
2026-01-01 23:05:05 +0100

    Mettre à jour la version en 2.4.2

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     )
 
     # App version
-    app_version: str = "2.4.1"
+    app_version: str = "2.4.2"
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent
