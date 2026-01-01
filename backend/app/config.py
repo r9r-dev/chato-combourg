a17e84b20e2ec4586c1274670225b98f62478644
r9r-dev <bold.spot3022@lamour.bzh>
2026-01-01 20:46:19 +0100

    Augmenter la version à 2.3.1

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     )
 
     # App version
-    app_version: str = "2.3.0"
+    app_version: str = "2.3.1"
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent
