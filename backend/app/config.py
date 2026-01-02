98bc5fd64025c6c4214033bb95f8d0a2820b4d1b
r9r-dev <bold.spot3022@lamour.bzh>
2026-01-02 03:23:03 +0100

    Passer à la version 3.0.0

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     )
 
     # App version
-    app_version: str = "2.4.4"
+    app_version: str = "3.0.0"
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent
