72793129328d26c5b1b5fb8d35f3ec3e98d2ac91
r9r-dev <bold.spot3022@lamour.bzh>
2026-01-02 15:56:19 +0100

    textes

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -65,7 +65,7 @@ export function Landing() {
                          shadow-lg shadow-gold/20
                          disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              Nouvelle partie
+              Enregistrer une partie
             </button>
 
             <button
@@ -80,7 +80,7 @@ export function Landing() {
                 <path d="M13 7H7v6h6V7z" />
                 <path fillRule="evenodd" d="M7 2a1 1 0 012 0v1h2V2a1 1 0 112 0v1h2a2 2 0 012 2v2h1a1 1 0 110 2h-1v2h1a1 1 0 110 2h-1v2a2 2 0 01-2 2h-2v1a1 1 0 11-2 0v-1H9v1a1 1 0 11-2 0v-1H5a2 2 0 01-2-2v-2H2a1 1 0 110-2h1V9H2a1 1 0 010-2h1V5a2 2 0 012-2h2V2zM5 5h10v10H5V5z" clipRule="evenodd" />
               </svg>
-              Jouer avec IA (Instable)
+              Jouer avec l'IA (version alpha)
             </button>
 
             <button
