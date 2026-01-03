8383c56d61369354afeff1f91686d5cd78cf0592
r9r-dev <bold.spot3022@lamour.bzh>
2026-01-03 12:31:29 +0100

    Bump version to 3.5.5

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     )
 
     # App version
-    app_version: str = "3.5.4"
+    app_version: str = "3.5.5"
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent
