606ea3746dea3c83db926e1342412d1b19050c55
r9r-dev <bold.spot3022@lamour.bzh>
2026-01-03 11:51:53 +0100

    Mise à jour version 3.5.3

--- backend/app/config.py
+++ backend/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     )
 
     # App version
-    app_version: str = "3.5.2"
+    app_version: str = "3.5.3"
 
     # Paths
     base_dir: Path = Path(__file__).parent.parent
